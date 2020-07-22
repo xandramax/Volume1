17a33eca62129f3f93c0ddd2dd6f2494856cfd5f
anlexmatos <anlexmatos@users.noreply.github.com>
2020-07-21 22:36:32 -0400

    Update release-notes.sh

--- scripts/release-notes.sh
+++ scripts/release-notes.sh
@@ -6,7 +6,6 @@ cat <<- EOH
 This release is automatically generated every time I push Algomorph
 to master. As such it is the latest version of the code and may be
 unstable, unusable, unsuitable for human consumption, and so on.
-The stable release is always available in the Rack Plugin Manager.
 
 These assets were built against
 https://vcvrack.com/downloads/Rack-SDK-1.0.0.zip
