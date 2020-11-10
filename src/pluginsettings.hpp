fa1cd3b633c8a63f71bae57dd174378b24ed2fab
anlexmatos <anlexmatos@users.noreply.github.com>
2020-11-10 16:58:27 -0500

    Saved Defaults: fixed for visual settings, added for aux input modes
    & tweak context menu text
    & add "Standard" preset

--- src/pluginsettings.hpp
+++ src/pluginsettings.hpp
@@ -3,11 +3,11 @@
 struct FMDelexandraSettings {
 	bool glowingInkDefault = false;
 	bool vuLightsDefault = true;
-	int auxInputDefaults[4] = {	0,
-								7,
-								9,
-								0};
+	bool allowMultipleModes[4] = {false};
+	// AuxInputModes::NUM_MODES = 18
+	bool auxInputDefaults[4][18] = {{false}};
 
+	FMDelexandraSettings();
 	void saveToJson();
 	void readFromJson();
 };
