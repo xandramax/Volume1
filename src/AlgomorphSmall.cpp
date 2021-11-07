e609194e578cd9d299d0d1e7d98fb4175b2362a2
Anthony Lexander Matos <anlexmatos@users.noreply.github.com>
2021-11-07 12:33:05 -0600

    Add comments

--- src/AlgomorphSmall.cpp
+++ src/AlgomorphSmall.cpp
@@ -518,16 +518,14 @@ void AlgomorphSmall::process(const ProcessArgs& args) {
                     INDICATOR_BRIGHTNESS
                     : 0.f, args.sampleTime * lightDivider.getDivision());
             }
-            // Duplicate??
-            // for (int i = 0; i < 3; i++) {
-            //     //Set purple component to off
-            //     lights[SCENE_LIGHTS + i * 3].setSmoothBrightness(0.f, args.sampleTime * lightDivider.getDivision());
-            // }
             //Set op/mod lights
             for (int i = 0; i < 4; i++) {
                 if (horizontalMarks[configScene].test(i)) {
                     if (modeB) {
                         //Set carrier indicator
+                        // In (config + alter ego) mode, when a selected operator is a forced-carrier,
+                        // the purple indicator should blink on/off whenever the yellow selection indicator blinks off/on,
+                        // regardless of whether it is horizontally marked.
                         if (forcedCarriers[configScene].test(i)) {
                             //Purple light
                             lights[CARRIER_INDICATORS + i * 3].setSmoothBrightness(configOp == i ?
@@ -566,6 +564,8 @@ void AlgomorphSmall::process(const ProcessArgs& args) {
                     }
                     else {
                         //Set carrier indicator
+                        // In config mode, when a selected operator is both a forced-carrier and disabled,
+                        // the red indicator should blink on/off whenever the yellow selection indicator blinks off/on.
                         if (forcedCarriers[configScene].test(i)) {
                             //Purple light
                             lights[CARRIER_INDICATORS + i * 3].setSmoothBrightness(0.f, args.sampleTime * lightDivider.getDivision());
@@ -605,6 +605,8 @@ void AlgomorphSmall::process(const ProcessArgs& args) {
                 }
                 else {
                     //Set carrier indicator
+                    // In config mode, if a non-horizontal-marked operator is a forced-carrier,
+                    // the purple indicator should blink on/off whenever the yellow selection indicator blinks off/on.
                     if (forcedCarriers[configScene].test(i)) {
                         //Purple light
                         lights[CARRIER_INDICATORS + i * 3].setSmoothBrightness(configOp == i ?
