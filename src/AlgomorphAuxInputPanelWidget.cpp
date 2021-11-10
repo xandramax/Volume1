a0f96445ea8c62757c1e2539434302be989ead12
System Delexandra <system@delexandra.com>
2021-11-10 07:59:28 -0600

    Scale auxPanelWidget brightness same as SvgLight
    Increase SvgLight min brightness

--- src/AlgomorphAuxInputPanelWidget.cpp
+++ src/AlgomorphAuxInputPanelWidget.cpp
@@ -13,6 +13,9 @@ void AlgomorphAuxInputPanelWidget::AlgoDrawWidget::drawLayer(const Widget::DrawA
 
     if (layer == 1) {
         font = APP->window->loadFont(rack::asset::plugin(pluginInstance, fontPath));
+
+        // Scale from max brightness to min brightness, as rack brightness is reduced from one to zero
+        nvgAlpha(args.vg, (1.f - SVG_LIGHT_MIN_ALPHA) * rack::settings::rackBrightness + SVG_LIGHT_MIN_ALPHA);
         
         // Draw labels
         nvgBeginPath(args.vg);
