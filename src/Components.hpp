4b1d7f5729433856ef9906ee7d5766dd8924bea1
Anthony Lexander Matos <anlexmatos@users.noreply.github.com>
2022-01-06 00:56:08 -0600

    Comment typos

--- src/Components.hpp
+++ src/Components.hpp
@@ -577,7 +577,7 @@ struct DLXSmallKnobLight : DLXKnobLight {
 	}
 };
 
-// The ratio of the height of a RoundSmallBlackKnob to the heught of a RoundHugeBlackKnob
+// The ratio of the height of a RoundSmallBlackKnob to the height of a RoundHugeBlackKnob
 constexpr float HOLE_RATIO = 4.145f / 7.123f;
 
 struct DLXDonutLargeKnobLight : DLXKnobLight {
